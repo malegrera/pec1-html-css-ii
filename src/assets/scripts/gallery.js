e395d388586933d12da06e1ea3e144bc55d6d119
malegrera <malegrera@uoc.edu>
2026-03-27 14:28:23 +0100

    estilos

--- src/assets/scripts/gallery.js
+++ src/assets/scripts/gallery.js
@@ -1,16 +1,15 @@
 import Glide from '@glidejs/glide';
-import '@glidejs/glide/dist/css/glide.core.min.css';
 
 const Gallery = () => {
-  const el = document.querySelector('.gallery');
-  if (!el) return;
+  const gallery = document.querySelector('.glide');
+  if (!gallery) return;
 
-  new Glide(el, {
+  new Glide(gallery, {
     type: 'carousel',
     startAt: 0,
     perView: 1,
     gap: 10,
-    autoplay: 3000, // opcional
+    autoplay: 3000, 
   }).mount();
 };
 
